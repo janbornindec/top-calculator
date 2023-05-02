afc4d74364f697c546ef7a4bbfa2227d4d245082
janbornindec <janellecwx@gmail.com>
2023-05-02 18:46:19 +1000

    Create func to add buttons

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+//add buttons onto calculator
+
+const controlPanel = document.querySelector('.controlPanel')
+
+function createButtons() {
+    for (let i=0; i<4; i++) {
+        const rows = document.createElement('div');
+        rows.setAttribute('class','rows');
+        controlPanel.appendChild(rows);
+        for (let x=0; x<4; x++) {
+            const buttons = document.createElement('div');
+            buttons.setAttribute('class','buttons');
+            rows.appendChild(buttons);
+        }
+    };
+};
+
+createButtons();
+
 //add function
 const add = (a,b) => a + b;
 //subtract function
@@ -7,7 +26,7 @@ const multiply = (a,b) => a * b;
 //divide function
 const divide = (a,b) => a / b;
 
-const operator;
+/*const operator;
 const firstNum;
 const secondNum;
 
@@ -23,5 +42,5 @@ const operate(firstNum,secondNum,operator) {
     } else {
         alert('Something went wrong. Please choose +, -, * or /')
     };
-};
+};*/
 
