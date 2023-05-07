b38034be99235a14d5272df372c6adc58ee2ebfa
janbornindec <janellecwx@gmail.com>
2023-05-07 13:08:49 +1000

    Display num value when clicked

--- script.js
+++ script.js
@@ -7,6 +7,20 @@ const multiply = (a,b) => a * b;
 //divide function
 const divide = (a,b) => a / b;
 
+const displayValue = document.querySelector('.display');
+const numButtons = document.querySelectorAll('.number');
+
+//store and display value when number button is clicked
+function displayNum() {
+    numButtons.forEach((numButton => {
+        numButton.addEventListener('click',() => {
+            displayValue.textContent += numButton.textContent;
+        });
+    }));
+};
+
+displayNum();
+
 /*const operator;
 const firstNum;
 const secondNum;
