a94d2144697b1101db65201216ba741f26a288a7
janbornindec <janellecwx@gmail.com>
2023-05-08 11:53:58 +1000

    Calculate the total of first two num to allow user to evaluate more than s ingle pair of numbers at a time

--- script.js
+++ script.js
@@ -15,13 +15,25 @@ function buttonListener() {
                 //if only 0 is on screen or last key clicked was operator then replace it with new num
                 if (displayedNum === '0' || previousKeyType === 'operator' ) {
                     display.textContent = keyContent;
+                    calculator.secondValue = display.textContent;
+                    console.log("Cal second value is " + calculator.secondValue);
+                    //store the current total of the first two num as temp
+                    temp = calculate(calculator.firstValue,calculator.operator,calculator.secondValue);
                 } else { //otherwise add new num after the existing num
                     display.textContent = displayedNum + keyContent;
                 };
                 previousKeyType = 'number';
             } else if (key.classList.contains('operator')) {
                 selectOperator(key);
-                calculator.firstValue = displayedNum;
+                //taking in the result from last equation as the first value
+                if (!calculator.firstValue || previousKeyType === 'calculate') {
+                    calculator.firstValue = displayedNum;
+                    console.log("Cal first value is " + calculator.firstValue);
+                } else {
+                    calculator.firstValue = temp; //use the current total as the first value
+                    console.log(calculator.firstValue);
+                    console.log(calculator.secondValue);
+                }
                 previousKeyType = 'operator';
             } else if (key.classList.contains('decimal')) {
                 console.log('Decimal key')
@@ -35,6 +47,7 @@ function buttonListener() {
             } else if (key.classList.contains('ac')) {
                 //clear display
                 display.textContent = '';
+                result = '';
                 //clear operator btn colour
                 operators.forEach((operator) => {
                     operator.classList.remove('active');
@@ -82,95 +95,3 @@ const calculate = (num1,operator,num2) => {
 };
 
 buttonListener();
-
-/*
-//add function
-const add = (a,b) => a + b;
-//subtract function
-const subtract = (a,b) => a - b;
-//multiply function
-const multiply = (a,b) => a * b;
-//divide function
-const divide = (a,b) => a / b;
-
-//get selected operator
-const opButtons = document.querySelectorAll('.operator')
-
-function displayOp() {
-    opButtons.forEach((opButton => {
-        opButton.addEventListener('click', () => {
-            display.textContent += opButton.textContent;
-            console.log(opButton.textContent);
-        });
-    }));
-};
-
-//store and display value when number button is clicked
-const display = document.querySelector('.display');
-const numButtons = document.querySelectorAll('.number');
-
-function displayNum() {
-    numButtons.forEach((numButton => {
-        numButton.addEventListener('click',() => {
-            display.textContent += numButton.textContent;
-            console.log(display.textContent);
-        });
-    }));
-};
-
-//set var for each parts of equation
-let numbers;
-let firstNum;
-let secondNum;
-
-let equation;
-
-function calculator() {
-    equation = display.textContent.split(/[^0-9.]/);
-    const initialValue = equation[0];
-    const solution = equation.reduce((operator,currentvalue) => lastValue  )
-}
-
-let solution;
-
-//decide which func to use
-function chooseFunc() {
-    if (operator === '+') {
-        solution = add(firstNum,secondNum);
-    } else if (operator === '-') {
-        solution = subtract(firstNum,secondNum);
-    } else if (operator === 'x') {
-        solution = multiply(firstNum,secondNum);
-    } else if (operator === '÷') {
-        solution = divide(firstNum,secondNum);
-    }
-}
-
-calculate the function to get solution
-const calculate = document.querySelector('.calculate');
-
-function operate() {
-    calculate.addEventListener('click',() => {
-        chooseFunc();
-        display.textContent = solution;
-    })
-};
-
-//clear display
-const clearBtn = document.querySelector('.ac');
-
-function clear() {
-    clearBtn.addEventListener('click',() => {
-        display.textContent = '';
-        equation = '';
-        numbers = [];
-        firstNum = '';
-        secondNum = '';
-    });
-}
-
-//trigger all funcs
-displayOp();
-displayNum();
-operate();
-clear();*/
